758d58e97984d6980c96ab5abe144d695742571a
Redomic <redomicmc@gmail.com>
2021-07-27 15:27:11 +0530

    modify section 16-20

--- Section 16-20/app.js
+++ Section 16-20/app.js
@@ -1,5 +1,13 @@
-console.log('start');
-// START
-let colors = ['red', 'green', 'blue'];
-// END
-console.log('stop');
+let guess = parseInt(prompt('Guess the number between 1-5'));
+let noOfGuesses = 0;
+let randomNum = Math.floor(Math.random() * 5) + 1;
+while (guess != randomNum) {
+    noOfGuesses++
+    if (guess > randomNum) {
+        guess = parseInt(prompt('Try Lower'));
+    } else if (guess < randomNum) {
+        guess = parseInt(prompt('Try Higher'));
+    } else {
+        guess = parseInt(prompt('Enter a valid number'));
+    }
+};
