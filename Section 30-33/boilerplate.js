ff3fb186e448a6b722e4a744a7d1977b336e7a71
Redomic <redomicmc@gmail.com>
2021-08-01 14:06:35 +0530

    add section 30-33

--- Section 30-33/boilerplate.js
+++ Section 30-33/boilerplate.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 
-let args = process.argv.slice(2);
-let fileName = args[0] || 'Project';
+let fileName = process.argv[2] || 'Project';
+let html = `./${fileName}/index.html`;
+let css = `./${fileName}/style.css`;
+let js = `./${fileName}/app.js`;
 
 try {
     fs.mkdirSync(fileName);
-
-    fs.writeFileSync(`${fileName}/index.html`);
-    fs.writeFileSync(`${fileName}/style.css`);
-    fs.writeFileSync(`${fileName}/app.js`);
+    fs.writeFileSync(html);
+    fs.writeFileSync(css);
+    fs.writeFileSync(js);
+    console.log('Finished Successfully');
 } catch (err) {
     console.log('Something went Wrong');
     console.log(err);
 }
-
-console.log('Finished Successfully');
