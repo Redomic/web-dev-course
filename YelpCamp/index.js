eb305ef2c66e16e5c351b4e46345dd4729717b9c
Redomic <redomicmc@gmail.com>
2021-08-13 14:35:47 +0530

    add section 41-42

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -6,6 +6,9 @@ const path = require('path');
 
 const Campground = require('./models/campground');
 
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -25,52 +28,60 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Middleware
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+// Routes response
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
-app.get('/campgrounds',  async (req, res) => {
+app.get('/campgrounds',  catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index.ejs', { campgrounds });
-})
+}))
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect('/campgrounds');
-})
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new.ejs');
 })
 
-app.get('/campgrounds/:id',  async (req, res) => {
+app.get('/campgrounds/:id',  catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show.ejs', { campground });
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${id}`);
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}))
 
-app.get('/campgrounds/:id/edit',  async (req, res) => {
+app.get('/campgrounds/:id/edit',  catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit.ejs', { campground });
-})  
+}))
 
+// Error Handling
+app.use((err, req, res, nex) => {
+    res.useCreateIndex('Something went wrong')
+})
+
+//Listener
 app.listen(3000, () => {
     console.log('Serving at port 3000')
 })
